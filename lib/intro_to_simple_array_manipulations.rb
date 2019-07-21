287053475c1d1138288a480140d2eaf73bd24398
Zermina Ejaz <zerminaejaz1018@icloud.com>
2019-07-21 04:28:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -7,7 +7,7 @@ def using_unshift(array, string)
 end
 
 def using_pop(array)
-  array.pop()
+  array.pop
 end
 
 def pop_with_args(array)
@@ -15,7 +15,7 @@ def pop_with_args(array)
 end
 
 def using_shift(array)
-  array.shift()
+  array.shift
 end
 
 def shift_with_args(array)
@@ -27,4 +27,8 @@ def using_concat(array1, array2)
 end
 
 def using_insert(array, element)
-  
+  array.insert(4, element)
+end
+
+def using_uniq(array)
+  array.uniq
